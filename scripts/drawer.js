2d60c3cd703719c6308dc879350cab500747a787
jack1-2-3 <jack.programming123@gmail.com>
2020-10-28 08:01:31 +0900

    drawer修正

--- scripts/drawer.js
+++ scripts/drawer.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
     $('.drawer').drawer();
     $('.drawer').on('drawer.opened', function() {
-        const scrollEvent = window.ontouchstart ? 'touchmove' : 'scroll';
-        $(window).on(scrollEvent, function() {
-            $('.drawer').drawer('close');
-        });
+        if(window.ontouchstart) {
+            $(window).touchmove(function() {
+                $('.drawer').drawer('close');
+            });
+        } else {
+            $(window).scroll(function() {
+                $('.drawer').drawer('close');
+            });
+        }
     });
 });
