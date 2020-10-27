733efaf08761c606a8e38916377cf8891e150118
jack1-2-3 <jack.programming123@gmail.com>
2020-10-28 06:14:33 +0900

    モバイルメニューにスクロール時の処理を追加

--- scripts/drawer.js
+++ scripts/drawer.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     $('.drawer').drawer();
+    $('.drawer').on('drawer.opened', function() {
+        $(window).scroll(function() {
+            $('.drawer').drawer('close');
+        });
+    });
 });
