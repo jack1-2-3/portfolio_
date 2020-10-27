c1c4f9e6ab45f8bd452f0805b4510d2632b84646
jack1-2-3 <jack.programming123@gmail.com>
2020-10-28 07:46:30 +0900

    drawer修正

--- scripts/drawer.js
+++ scripts/drawer.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('.drawer').drawer();
     $('.drawer').on('drawer.opened', function() {
-        const scrollEvent = window.ontouchstart ? 'onscroll' : 'scroll';
+        const scrollEvent = window.ontouchstart ? 'touchmove' : 'scroll';
         $(window).on(scrollEvent, function() {
             $('.drawer').drawer('close');
         });
