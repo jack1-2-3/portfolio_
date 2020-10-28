2598def5b207d14ff2fa023ecb6b0eadccc09817
jack1-2-3 <jack.programming123@gmail.com>
2020-10-28 09:11:13 +0900

    drawerにスクロール時のイベントを追加

--- scripts/drawer.js
+++ scripts/drawer.js
@@ -1,14 +1,8 @@
 $(document).ready(function() {
     $('.drawer').drawer();
     $('.drawer').on('drawer.opened', function() {
-        if(window.ontouchstart) {
-            $('.drawer').touchmove(function() {
-                $('.drawer').drawer('close');
-            });
-        } else {
-            $(window).scroll(function() {
-                $('.drawer').drawer('close');
-            });
-        }
+        $(window).scroll(function() {
+            $('.drawer').drawer('close');
+        });
     });
 });
