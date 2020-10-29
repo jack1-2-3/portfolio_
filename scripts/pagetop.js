48433cdd8b3cdc2311643f1cc9676c689bab7b78
ジャック <jack@jakkunoMacBook-Pro.local>
2020-10-29 10:56:44 +0900

    pagetopボタンを実装

---
+++ scripts/pagetop.js
@@ -0,0 +1,11 @@
+$(function () {
+    const topBtn = $('#page-top');
+    topBtn.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+});
